676142797f227a9dbc874024be81c28457ca1f86
Mauro Moya <99930598+MauroMoyaUCB@users.noreply.github.com>
2022-10-31 02:14:12 -0400

    fix: dependency

--- src/main/java/singleton/SingletonMain.java
+++ src/main/java/singleton/SingletonMain.java
@@ -3,6 +3,7 @@ package singleton;
 public class SingletonMain {
 
     public static void main(String[]arg){
+/*
         MongoDBClient mongoDBClient = MongoDBClient.getClient();
         System.out.println(mongoDBClient.getData());
         MongoDBClient mongoDBClient2 = MongoDBClient.getClient();
@@ -10,6 +11,14 @@ public class SingletonMain {
         MongoDBClient mongoDBClient3 = MongoDBClient.getClient();
         System.out.println(mongoDBClient3.getData());
 
+*/
+
+
+        MySqlSingleton mySqlSingleton = (MySqlSingleton) MySqlSingleton.getConnection();
+        System.out.println(mySqlSingleton.getConnection());
+
+        MySqlSingleton mySqlSingleton2 = (MySqlSingleton) MySqlSingleton.getConnection();
+        System.out.println(mySqlSingleton2.getConnection());
 
     }
 }
